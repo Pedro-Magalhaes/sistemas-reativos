c483da1adf438e8bcbe667a2f44966a6078e902d
Conta do Software Arduino <arduino@robin.grad.inf.puc-rio.br>
2019-03-26 18:30:55 -0300

    Versao inicial do mini projeto

---
+++ Arduino/mini-projeto/event_driven/app.ino
@@ -0,0 +1,60 @@
+/* Define shift register pins used for seven segment display */
+#define LATCH_DIO 4
+#define CLK_DIO 7
+#define DATA_DIO 8
+ 
+/* Segment byte maps for numbers 0 to 9 */
+const byte SEGMENT_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};
+/* Byte maps to select digit 1 to 4 */
+const byte SEGMENT_SELECT[] = {0xF1,0xF2,0xF4,0xF8};
+
+byte timeVector[4] = { 0,0,0,0 }; 
+ 
+
+void appinit(void) {
+    pinMode(LATCH_DIO,OUTPUT);
+    pinMode(CLK_DIO,OUTPUT);
+    pinMode(DATA_DIO,OUTPUT);
+    for (int i = 0; i < 4; i++) {
+      WriteNumberToSegment(i , timeVector[i]);
+    }
+    timer_set(0,60000);
+}
+void button_changed ( int pin, int v);
+
+
+void timer_expired(void) {
+    updateTimeVector();
+    for (int i = 0; i < 4; i++) {
+      WriteNumberToSegment(i , timeVector[i]);
+    }
+    timer_set(0,60000);
+}
+
+
+void updateTimeVector() {
+    unsigned long int currTime = timeVector[0] * 600 + timeVector[1] * 60 + timeVector[2] * 10 + timeVector[3] + 1;
+    mapTimeToVector(currTime);
+    
+}
+
+void mapTimeToVector( unsigned long int currTime ) {
+    unsigned long int h = currTime / 60;
+    unsigned long int m = currTime % 60; 
+
+    timeVector[0] = h/10;
+    timeVector[1] = h%10;
+    timeVector[2] = m/10;
+    timeVector[3] = m%10;  
+//    Serial.println( String(currTime) + "    " + String(h) + "    " + String(m));
+}
+ 
+/* Write a decimal number between 0 and 9 to one of the 4 digits of the display */
+void WriteNumberToSegment(byte Segment, byte Value) {
+  digitalWrite(LATCH_DIO,LOW);
+  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[Value]);
+  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[Segment] );
+  digitalWrite(LATCH_DIO,HIGH);
+}
+
+
---
+++ Arduino/mini-projeto/event_driven/event_driven.ino
@@ -0,0 +1,78 @@
+
+#include "event_driven.h"
+#include "pindefs.h"
+#include "app.h"
+#define B_TIMEOUT 50
+
+bool listenButtons[3] = { false,false,false };
+int lastButtonRead[3] = { HIGH, HIGH, HIGH };
+unsigned short int buttonsTimeout[] = { B_TIMEOUT, B_TIMEOUT, B_TIMEOUT };
+unsigned short int buttonsCurrTime[] = { 0, 0, 0 };
+unsigned int timer = 0;
+unsigned long int whenTimeWasSet = 0;
+
+
+void button_listen (int pin) {
+  if( pin == BUTTON1 ){
+     listenButtons[0] = true;
+  }  
+  else if( pin==BUTTON2 ){
+    listenButtons[1] = true;
+  }
+  else if( pin == BUTTON3 ) {
+    listenButtons[2] = true;
+  }
+}
+void timer_set (int n,int ms) {
+  whenTimeWasSet = millis();
+  timer = ms;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUTTON1,INPUT_PULLUP);
+  pinMode(BUTTON2,INPUT_PULLUP);
+  pinMode(BUTTON3,INPUT_PULLUP);  
+  appinit();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int but1 = digitalRead(BUTTON1);
+  int but2 = digitalRead(BUTTON2);
+  int but3 = digitalRead(BUTTON3);
+
+//  int but1Read = digitalRead(BUTTON1);
+//
+//  if (but1Read != lastButtonRead[0]){
+//    oscilationBut1 = millis()
+//  }
+//  if (millis() - oscilationBut1 >= DEBOUNCE && but1Read !=lastButtonRead[0]){
+//    but1 = but1Read;
+//  }
+  
+  unsigned long now = millis(); 
+  if( listenButtons[0] && ( but1 != lastButtonRead[0] ) && ( now >= buttonsCurrTime[0] + buttonsTimeout[0] )  ) {
+    lastButtonRead[0] = but1;
+    buttonsCurrTime[0] = now;
+    button_changed(BUTTON1,but1);
+  }
+  if( listenButtons[1] && ( but2 != lastButtonRead[1] ) && ( now >= buttonsCurrTime[1] + buttonsTimeout[1] )  ) {
+    lastButtonRead[1] = but2;
+    buttonsCurrTime[1] = now;
+    button_changed(BUTTON2,but2);
+  }
+  if( listenButtons[2] && ( but3 != lastButtonRead[2] ) && ( now >= buttonsCurrTime[2] + buttonsTimeout[2] )  ) {
+    buttonsCurrTime[2] = now;
+    lastButtonRead[2] = but3;
+    button_changed(BUTTON2,but3);
+  }
+  if( timer && ( now >= whenTimeWasSet + timer) ) {
+    timer = 0;
+    timer_expired();
+  }
+  
+  
+  
+}
